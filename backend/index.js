909800b500fdb39cb14b76b17fcb011351b201fb
Charlie Kim <charlie2@ualberta.ca>
2024-07-14 15:27:42 -0600

    part3c

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
-morgan.token("body", (req, res) => {
+morgan.token("body", (req, _) => {
   return JSON.stringify(req.body);
 });
 app.use(
@@ -14,79 +16,95 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("./backend/dist"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", (_, res) => {
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", (_, res) => {
   const date = new Date();
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`,
-  );
+  Person.find({}).then((persons) => {
+    res.send(
+      `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`,
+    );
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(id).then((person) => {
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+  Person.findByIdAndDelete(id)
+    .then((_) => {
+      res.status(204).end();
+    })
+    .catch((e) => e);
 });
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-  const person = persons.find((person) => person.name === body.name);
-  if (person) {
-    return res.status(400).json({
-      error: "person already exists in the phonebook",
-    });
-  }
   if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "name or number is missing",
-    });
+    res.status(400);
+    res.send("name or number missing");
   }
-  const newPerson = {
-    id: Math.floor(Math.random() * 1000).toString(),
-    name: body.name,
-    number: body.number,
-  };
-  persons = persons.concat(newPerson);
-  console.log(persons);
-  res.json(newPerson);
+  Person.find({ name: body.name }).then((person) => {
+    if (person.length > 0) {
+      res.status(400);
+      res.send("name must be unique");
+      return;
+    }
+    const p = new Person({
+      name: body.name,
+      number: body.number,
+    });
+    p.save().then((savedPerson) => {
+      res.json(savedPerson);
+    });
+  });
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.put("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  Person.findById(id).then((person) => {
+    if (!person) {
+      res.status(404).end();
+      return;
+    }
+    person.name = body.name;
+    person.number = body.number;
+    person
+      .save()
+      .then((updatedPerson) => {
+        res.json(updatedPerson);
+      })
+      .catch((e) => next(e));
+  });
+});
+
+const errorHandler = (err, res, _, next) => {
+  console.errer(err.message);
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ error: err.message });
+  }
+  next(err);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
